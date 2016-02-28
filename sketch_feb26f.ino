c3b92b7340d43372e03a99d69e8af07b4becd274
Rahul Patni <patnir@users.noreply.github.com>
2016-02-28 12:31:23 -0500

    Added files via upload

---
+++ sketch_feb26f.ino
@@ -0,0 +1,20 @@
+const int ledPin = 13;
+const int button = 2;
+void setup() {
+  // put your setup code here, to run once:
+  //Serial.begin(9600);
+  pinMode(ledPin, OUTPUT);
+  pinMode(button, INPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+   if (digitalRead(button) == 1) {
+      digitalWrite(ledPin, LOW);
+   }
+   delay(30);
+   if (digitalRead(button) == 0) {
+    digitalWrite(ledPin, HIGH);
+   }
+   delay(30);
+}
