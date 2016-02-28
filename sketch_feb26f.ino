2b46460bd43020450eb740155d5bd46b54ddb203
Rahul Patni <patnir@users.noreply.github.com>
2016-02-28 12:32:59 -0500

    Update sketch_feb26f.ino

--- sketch_feb26f.ino
+++ sketch_feb26f.ino
@@ -2,17 +2,21 @@ const int ledPin = 13;
 const int button = 2;
 void setup() {
   // put your setup code here, to run once:
-  //Serial.begin(9600);
+  // Uses the pin as output
   pinMode(ledPin, OUTPUT);
+  // button is used as input
   pinMode(button, INPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  // if the button is not pressed, turn the light off
    if (digitalRead(button) == 1) {
       digitalWrite(ledPin, LOW);
    }
+   //delay to improve consistency
    delay(30);
+   // if the button is pressed, turn the light off
    if (digitalRead(button) == 0) {
     digitalWrite(ledPin, HIGH);
    }
